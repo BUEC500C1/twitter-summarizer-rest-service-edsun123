cfcbb3a79711949f062b37d8cc99a6a601d051e6
Edwin Sun <jennasun@dhcp-wifi-8021x-155-41-102-43.bu.edu>
2020-02-28 11:42:32 -0500

    modified test file

---
+++ test_file_exist.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def random_test():
+    assert 1==1
+    
+def does_keys_exist():
+    assert Path('keys.py').is_file() == 1
+    
+def does_mergedfile_exist():
+     assert Path('mergedfile.mp4').is_file() == 1
+     
+def does_requirements_exist():
+    assert Path('requirements.txt').is_file() == 1
+
+def does_list_exist():
+    assert Path('list.txt').is_file() == 1
+    
+def does_list_exist():
+    assert Path('tweet_vid_converter').is_file() == 1
