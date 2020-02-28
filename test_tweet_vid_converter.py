9da4e9914dc7ed8897a370cf2d6f844bfc17b97e
Edwin Sun <jennasun@dhcp-wifi-8021x-155-41-102-43.bu.edu>
2020-02-28 14:59:48 -0500

    fixed pytest

--- test_tweet_vid_converter.py
+++ test_tweet_vid_converter.py
@@ -1,20 +1,22 @@
-# from pathlib import Path
-
-
-def random_test():
+from pathlib import Path
+#from flask import Flask
+#import pytest
+#app = Flask(__name__)
+#
+def test_random_test():
     assert 1==1
     
-# def does_keys_exist():
-#     assert Path('keys.py').is_file()
+def test_does_keys_exist():
+    assert Path('keys.py').is_file()
     
-# def does_mergedfile_exist():
-#      assert Path('mergedfile.mp4').is_file()
+def test_does_mergedfile_exist():
+    assert Path('mergedfile.mp4').is_file()
      
-# def does_requirements_exist():
-#     assert Path('requirements.txt').is_file()
+def test_does_requirements_exist():
+    assert Path('requirements.txt').is_file()
 
-# def does_list_exist():
-#     assert Path('list.txt').is_file()
+def test_does_list_exist():
+    assert Path('list.txt').is_file()
     
-# def does_list_exist():
-#     assert Path('tweet_vid_converter').is_file()
+def test_does_list_exist():
+    assert Path('tweet_vid_converter.py').is_file()
