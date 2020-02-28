d33d7d39048a119fd7eee9c34d3fa4c9a5e87ab4
edsun123 <34005715+edsun123@users.noreply.github.com>
2020-02-28 13:37:04 -0500

    Update test_file_exist.py

--- test_file_exist.py
+++ test_file_exist.py
@@ -4,17 +4,17 @@ from pathlib import Path
 def random_test():
     assert 1==1
     
-def does_keys_exist():
-    assert Path('keys.py').is_file()
+# def does_keys_exist():
+#     assert Path('keys.py').is_file()
     
-def does_mergedfile_exist():
-     assert Path('mergedfile.mp4').is_file()
+# def does_mergedfile_exist():
+#      assert Path('mergedfile.mp4').is_file()
      
-def does_requirements_exist():
-    assert Path('requirements.txt').is_file()
+# def does_requirements_exist():
+#     assert Path('requirements.txt').is_file()
 
-def does_list_exist():
-    assert Path('list.txt').is_file()
+# def does_list_exist():
+#     assert Path('list.txt').is_file()
     
-def does_list_exist():
-    assert Path('tweet_vid_converter').is_file()
+# def does_list_exist():
+#     assert Path('tweet_vid_converter').is_file()
